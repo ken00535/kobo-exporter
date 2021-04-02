c1aab8342279fb9ccf6f223a8f471de13a5c7771
kenwschen <ken00535@gmail.com>
2021-04-03 02:01:27 +0800

    fix(main): docx format

--- kobo_exporter/__main__.py
+++ kobo_exporter/__main__.py
@@ -1,6 +1,7 @@
 import docx
 from docx.shared import Pt, RGBColor
 from docx.oxml.ns import qn
+from docx.enum.text import WD_LINE_SPACING
 from kobo_exporter.model.model import Handler
 
 
@@ -14,10 +15,17 @@ for bookmark in bookmarks:
     text.style.font.name = u'新細明體'
     text.style._element.rPr.rFonts.set(qn('w:eastAsia'), u'新細明體')
     text.style.font.color.rgb = RGBColor(0x00, 0x70, 0xC0)
+    text.paragraph_format.space_before = Pt(0)
+    text.paragraph_format.space_after = Pt(0)
+    text.paragraph_format.line_spacing_rule = WD_LINE_SPACING.SINGLE
     annotation = handler.format_annotation(bookmark)
     if annotation != '':
         run = text.add_run(handler.format_annotation(bookmark))
         run.font.color.rgb = RGBColor(0x00, 0x00, 0x00)
-    doc.add_paragraph('')
+    text = doc.add_paragraph('')
+    text.paragraph_format.space_before = Pt(0)
+    text.paragraph_format.space_after = Pt(0)
+    text.paragraph_format.line_spacing_rule = WD_LINE_SPACING.SINGLE
+
 
 doc.save('note.docx')
